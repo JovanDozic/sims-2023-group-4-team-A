95b6e81ebf1d125ec9e4b80781184a032c3f6e94
Natalija Popović <popovic.ra117.2020@uns.ac.rs>
2023-03-08 22:49:24 +0100

    Tour and KeyPoint models created

---
+++ sims-2023-group-4-team-A/SIMSProject/Model/KeyPoint.cs
@@ -0,0 +1,40 @@
+﻿using SIMSProject.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIMSProject.Model
+{
+    public class KeyPoint : ISerializable
+    {
+        public int Id { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public int TourId { get; set; }
+
+        public KeyPoint()
+        {
+            
+        }
+        public KeyPoint(int id, string description, int tourId)
+        {
+            Id = id;
+            Description = description;
+            TourId = tourId;
+        }
+
+        public string[] ToCSV()
+        {
+            string[] csvValues = { Id.ToString(), Description, TourId.ToString() };
+            return csvValues;
+        }
+
+        public void FromCSV(string[] values)
+        {
+            Id = Convert.ToInt32(values[0]);
+            Description = values[1];
+            Id = Convert.ToInt32(values[2]);
+        }
+    }
+}
