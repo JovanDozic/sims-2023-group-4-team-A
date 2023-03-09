0c49443e1c53114d061e7edf06765964cebafabc
Natalija Popović <popovic.ra117.2020@uns.ac.rs>
2023-03-09 23:05:48 +0100

    TourLocation class created

---
+++ sims-2023-group-4-team-A/SIMSProject/Model/TourLocation.cs
@@ -0,0 +1,39 @@
+﻿using SIMSProject.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIMSProject.Model
+{
+    public class TourLocation : ISerializable
+    {
+        public int Id { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+
+        public TourLocation(int id, string city, string country)
+        {
+            Id = id;
+            City = city;
+            Country = country;
+        }
+
+        public string[] ToCSV()
+        {
+            string[] csvValues = {Id.ToString(), City.ToString(), Country.ToString()};
+            return csvValues;
+        }
+
+        public void FromCSV(string[] values)
+        {
+            Id = Convert.ToInt32(values[0]);
+            City = Convert.ToString(values[1]);
+            Country = Convert.ToString(values[2]);
+        }
+    }
+
+    
+}
