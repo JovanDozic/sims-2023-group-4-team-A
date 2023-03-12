ab090ba78fc1f4ab852104eaa9446c5118e83fc0
JovanDozic <jovandozic@gmail.com>
2023-03-12 16:02:14 +0100

    Minor changes and testing

--- sims-2023-group-4-team-A/SIMSProject/Model/User.cs
+++ sims-2023-group-4-team-A/SIMSProject/Model/User.cs
@@ -8,6 +8,7 @@ namespace SIMSProject.Model
         public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
+        
 
         public User() { }
 
