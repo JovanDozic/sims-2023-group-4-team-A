be5b841553794ec05063a9fdfe1622dded58b8c4
Anja Maksimovic <maksimovicanja11@gmail.com>
2023-03-14 13:22:58 +0100

    Numeric text boxes done

--- sims-2023-group-4-team-A/SIMSProject/View/Guest2/ShowAndSearchTours.xaml.cs
+++ sims-2023-group-4-team-A/SIMSProject/View/Guest2/ShowAndSearchTours.xaml.cs
@@ -17,8 +17,8 @@ using System.Windows.Shapes;
 using SIMSProject.Controller;
 using System.Collections;
 using System.Diagnostics;
-
-
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace SIMSProject.View.Guest2
 {
@@ -29,16 +29,38 @@ namespace SIMSProject.View.Guest2
 
     public enum TourLanguage { ENGLISH = 0, SERBIAN, SPANISH, FRENCH };
 
-    public partial class ShowAndSearchTours : Window
+    public partial class ShowAndSearchTours : Window, INotifyPropertyChanged
     {
         public Tour Tour { get; set; }
-        public ObservableCollection<Tour> Tours { get; set; }
-        //public ObservableCollection<TourLocation> TourLocations { get; set; }      
-        //public ObservableCollection<Tour> toursFilteredByLocation { get; set; }
-        //public ObservableCollection<Tour> toursFilteredByDuration { get; set; }
-        //public ObservableCollection<Tour> toursFilteredByLanguage { get; set; }
-        //public ObservableCollection<Tour> toursFilteredByMaxGuests { get; set; }
 
+        private int _durationSearchBox = 0;
+        public int DurationSearchBox
+        {
+            get => _durationSearchBox;
+            set
+            {
+                if (value != _durationSearchBox && value >= 0)
+                {
+                    _durationSearchBox = value;
+                    OnPropertyChanged(nameof(DurationSearchBox));
+                }
+            }
+        }
+
+        private int _maxGuestSearchBox = 0;
+        public int MaxGuestSearchBox
+        {
+            get => _maxGuestSearchBox;
+            set
+            {
+                if (value != _maxGuestSearchBox && value >= 0)
+                {
+                    _maxGuestSearchBox = value;
+                    OnPropertyChanged(nameof(MaxGuestSearchBox));
+                }
+            }
+        }
+        public ObservableCollection<Tour> Tours { get; set; }
 
         private readonly TourController TourController;
         private readonly TourLocationController TourLocationController = new();
@@ -51,8 +73,7 @@ namespace SIMSProject.View.Guest2
             DataContext = this;
             Tour = new();
             TourController = new TourController();
-            //tourController.Subscribe(this);
-
+            
             Tours = new ObservableCollection<Tour>(TourController.GetAll());
 
             var tourLocations = TourLocationController.GetAll();
@@ -61,11 +82,10 @@ namespace SIMSProject.View.Guest2
             {
                 tour.Location = tourLocations.Find(x => x.Id == tour.LocationId);
             }
-
-            //Trace.WriteLine("lok " + Tours.First().LocationId + TourLocationController.GetAll().Find(x => x.Id == Tours.First().LocationId));
+            
         }
 
-        private void PretraziClick(object sender, RoutedEventArgs e)
+        private void Search_Click(object sender, RoutedEventArgs e)
         {
             Tours.Clear();
             foreach (var tour in new ObservableCollection<Tour>
@@ -89,14 +109,12 @@ namespace SIMSProject.View.Guest2
 
             // Removing by numbers
             if (searchDuration > 0) searchResults.RemoveAll(x => x.Duration != searchDuration);
-            if(searchMaxGuests > 0) searchResults.RemoveAll(x=>x.MaxGuestNumber < searchMaxGuests);
-          
+            if(searchMaxGuests > 0) searchResults.RemoveAll(x=>x.MaxGuestNumber < searchMaxGuests);          
 
             Tours.Clear();
             foreach (var searchResult in searchResults)
                 Tours.Add(searchResult);
             
-
         }
 
         private void TextSearch_GotFocus(object sender, RoutedEventArgs e)
@@ -119,36 +137,11 @@ namespace SIMSProject.View.Guest2
             }
         }
 
-        private void SearchTours(int numGuests, Language language, string location, int duration)
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
-            // TODO: Implement code to search for tours using the provided parameters
-            List<Tour> matchingTours = new List<Tour>();
-
-            foreach (Tour tour in Tours)
-            {
-                // Check if the tour matches the search criteria
-                if ((numGuests == 0 || tour.MaxGuestNumber >= numGuests) &&
-                    /*(language == Language.Any || tour.TourLanguage == language) &&*/
-                    (string.IsNullOrEmpty(location) || tour.Location.Equals(location)) &&
-                    (duration == 0 || tour.Duration <= duration))
-                {
-                    // Add the tour to the list of matching tours
-                    matchingTours.Add(tour);
-                }
-            }
-
-            Tours.Clear();
-            foreach (Tour tour in matchingTours)
-            {
-                Tours.Add(tour);
-            }
-
-            //tours = matchingTours;
-
-            // Display the matching tours to the user
-            //ShowMatchingTours(matchingTours);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
-
 
     }
 }
