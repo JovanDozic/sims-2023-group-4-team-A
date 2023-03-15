ac18cc406ff1a7e31f4d3d36a00dfbeacaad18ac
JovanDozic <jovandozic@gmail.com>
2023-03-15 14:58:36 +0100

    [Namespace Error Fix]

--- sims-2023-group-4-team-A/SIMSProject/View/InitialWindow.xaml.cs
+++ sims-2023-group-4-team-A/SIMSProject/View/InitialWindow.xaml.cs
@@ -1,29 +1,13 @@
 using System.Windows;
 using SIMSProject.View.OwnerViews;
-using SIMSProject.Controller;
-using SIMSProject.Model;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using SIMSProject.View.GuideViews;
 using SIMSProject.View.Guest2;
 using SIMSProject.View.Guest1;
-using SIMSProject.View.GuideViews;
-
 
 namespace SIMSProject.View
 {
-    /// <summary>
-    /// Interaction logic for InitialWindow.xaml
-    /// </summary>
     public partial class InitialWindow : Window
     {
-
-        public Accommodation Accommodation { get; set; }
-        public static AccommodationController AccommodationController { get; set; }
         public InitialWindow()
         {
             InitializeComponent();
@@ -43,16 +27,14 @@ namespace SIMSProject.View
 
         private void Owner_Click(object? sender, RoutedEventArgs? e)
         {
-            OwnerInitialWindow window = new();
-            window.Show();
+            OwnerInitialWindow ownerInitialWindow = new();
+            ownerInitialWindow.Show();
         }
 
         private void TourGuide_Click(object sender, RoutedEventArgs e)
         {
             TourCreation tourCreation = new TourCreation();
             tourCreation.Show();
-            
-            
         }
     }
 }
