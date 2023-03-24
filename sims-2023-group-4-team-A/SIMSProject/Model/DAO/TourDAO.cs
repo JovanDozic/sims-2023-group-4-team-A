f36f283841f7ba97a14b76ac285b9e84c2e16c8d
Natalija Popović <popovic.ra117.2020@uns.ac.rs>
2023-03-24 19:17:45 +0100

    Added namespace for Guide in TourDAO

--- sims-2023-group-4-team-A/SIMSProject/Model/DAO/TourDAO.cs
+++ sims-2023-group-4-team-A/SIMSProject/Model/DAO/TourDAO.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using SIMSProject.FileHandler;
+using SIMSProject.Model.UserModel;
 using SIMSProject.Observer;
 
 namespace SIMSProject.Model.DAO
