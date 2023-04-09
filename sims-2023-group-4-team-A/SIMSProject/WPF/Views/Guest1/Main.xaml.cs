7b2656bcffd32d8cb93a67bdb1ac105e4ee2f2e0
Marko Košarac <marko_kosarac@hotmail.com>
2023-04-09 15:25:30 +0200

    Created main page that will appear when application is started

--- sims-2023-group-4-team-A/SIMSProject/WPF/Views/Guest1/Main.xaml.cs
+++ sims-2023-group-4-team-A/SIMSProject/WPF/Views/Guest1/Main.xaml.cs
@@ -13,41 +13,36 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Windows.Navigation;
 using SIMSProject.View;
+using System.ComponentModel;
 
 namespace SIMSProject.WPF.Views.Guest1
 {
     /// <summary>
     /// Interaction logic for Main.xaml
     /// </summary>
-    public partial class Main : Window
+    public partial class Main : Window, INotifyPropertyChanged
     {
-        private NavigationBar navigationBar;
-        private NavigationService _navigationService;
         public Main()
         {
             InitializeComponent();
+            OpenMainPage();
         }
 
-
+        
+        public void OpenMainPage()
+        {
+            MainWind.Content = new MainPage();
+        }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             MainWind.Content = new Probna();
         }
-
-        private void Button_Click_2(object sender, RoutedEventArgs e)
+        private void Button_Click_Home(object sender, RoutedEventArgs e)
         {
-            if (navigationBar == null)
-            {
-                navigationBar = new NavigationBar(); // create a new instance of the NavigationBar page
-                MainWind.Content = navigationBar; // set the NavigationBar page as the content of the MainWindow
-            }
-            else
-            {
-                MainWind.Content = null; // remove the NavigationBar page from the content of the MainWindow
-                navigationBar = null; // set the reference to the NavigationBar page to null to allow for a new instance to be created if the button is clicked again
-            }
+            MainWind.Content = new MainPage();
         }
+
 
         private void Button_Click_Close(object sender, RoutedEventArgs e)
         {
@@ -56,16 +51,18 @@ namespace SIMSProject.WPF.Views.Guest1
             open.Show();
             
         }
-        /*
-        private void ReturnToMainWindow_Click(object sender, RoutedEventArgs e)
+
+        private void BG_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            // Vratite se na početnu stranicu (prvi unos) u istoriji navigacije
-            while (MainWind.NavigationService.CanGoBack)
-            {
-                MainWind.NavigationService.RemoveBackEntry();
-            }
-            MainWind.NavigationService.Navigate(new Uri("Page1.xaml", UriKind.Relative));
+            ButtonMenu.IsChecked = false;
         }
-        */
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
     }
 }
