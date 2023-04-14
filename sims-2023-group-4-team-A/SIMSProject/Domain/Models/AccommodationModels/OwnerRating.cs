d7f3e9a7ab403fc138498a17a15fbc21a6570521
JovanDozic <jovandozic@gmail.com>
2023-04-14 12:59:36 +0200

    [OwnerRating basic model created]
    OwnerRating model has been created, along with a corresponding CSV file and file handler. Additionally, an ImageSerializer class was created to reduce the redundancy of image URL string generators in model serializer functions.

---
+++ sims-2023-group-4-team-A/SIMSProject/Domain/Models/AccommodationModels/OwnerRating.cs
@@ -0,0 +1,58 @@
+﻿using SIMSProject.Serializer;
+using System.Collections.Generic;
+
+namespace SIMSProject.Domain.Models.AccommodationModels
+{
+    public class OwnerRating : ImageSerializer, ISerializable
+    {
+        public int Id { get; set; }
+        public AccommodationReservation AccommodationReservation { get; set; } = new();
+        public int CleanlinessRating { get; set; } = 1;
+        public int OwnerCorrectness { get; set; } = 1;
+        public int Kindness { get; set; } = 1;
+        public string Comment { get; set; } = string.Empty;
+        public List<string> ImageURLs { get; set; } = new();
+        public string ImageURLsCSV { get; set; } = string.Empty;
+
+        public OwnerRating()
+        {
+        }
+
+        public OwnerRating(int id, AccommodationReservation accommodationReservation, int cleanlinessRating, int ownerCorrectness, string comment, List<string> imageURLs, string imageURLsCSV)
+        {
+            Id = id;
+            AccommodationReservation = accommodationReservation;
+            CleanlinessRating = cleanlinessRating;
+            OwnerCorrectness = ownerCorrectness;
+            Comment = comment;
+            ImageURLs = imageURLs;
+            ImageURLsCSV = imageURLsCSV;
+        }
+
+        public string[] ToCSV()
+        {
+            ImageURLsCSV = ImageURLsToCSV(ImageURLs);
+            string[] csvValues =
+            {
+                Id.ToString(),
+                AccommodationReservation.Id.ToString(),
+                CleanlinessRating.ToString(),
+                OwnerCorrectness.ToString(),
+                Comment,
+                ImageURLsCSV
+            };
+            return csvValues;
+        }
+
+        public void FromCSV(string[] values)
+        {
+            Id = int.Parse(values[0]);
+            AccommodationReservation.Id = int.Parse(values[1]);
+            CleanlinessRating = int.Parse(values[2]);
+            OwnerCorrectness = int.Parse(values[3]);
+            Comment = values[4];
+            ImageURLsCSV = values[5];
+            ImageURLs = ImageURLsFromCSV(ImageURLsCSV);
+        }
+    }
+}
