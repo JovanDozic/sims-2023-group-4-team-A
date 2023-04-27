6099c5d78936fe771ab6c9cf90daa4ca1dd38f0b
Marko Košarac <marko_kosarac@hotmail.com>
2023-04-27 10:57:02 +0200

    Created slider with buttons but it needs to be styled

--- sims-2023-group-4-team-A/SIMSProject/WPF/CustomControls/ImageSlider.xaml.cs
+++ sims-2023-group-4-team-A/SIMSProject/WPF/CustomControls/ImageSlider.xaml.cs
@@ -1,5 +1,4 @@
-﻿using SIMSProject.WPF.ViewModels.AccommodationViewModels;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Windows.Xps.Serialization;
 
 namespace SIMSProject.WPF.CustomControls
 {
@@ -21,7 +22,74 @@ namespace SIMSProject.WPF.CustomControls
     /// </summary>
     public partial class ImageSlider : UserControl
     {
-       
+        private List<string> Images = new List<string>();
+        private int currentIndex;
+        public ImageSlider()
+        {
+            InitializeComponent();
+            currentIndex = 0;
+            LeftButton.IsEnabled = false;
+            RightButton.IsEnabled = true;
+
+            LeftButton.Click += Button_Click_Previous;
+            RightButton.Click += Button_Click_Next;
+
+        }
+
+        public void AddImages(List<string> imageUrls)
+        {
+            Images.AddRange(imageUrls);
+            if (Images.Count == 1)
+            {
+                UpdateImage();
+            }
+            else
+            {
+                UpdateImage();
+                RightButton.IsEnabled = true;
+            }
+        }
+        public void AddImage(string imageUrl)
+        {
+            Images.Add(imageUrl);
+            if (Images.Count == 1)
+            {
+                UpdateImage();
+            }
+        }
+        private void UpdateImage()
+        {
+            if (Images.Count > 0)
+            {
+                BitmapImage bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.UriSource = new Uri(Images[currentIndex], UriKind.Absolute);
+                bitmapImage.EndInit();
+                ImageControl.Source = bitmapImage;
+            }
+        }
+
+        private void Button_Click_Previous(object sender, RoutedEventArgs e)
+        {
+            currentIndex--;
+            if (currentIndex == 0)
+            {
+                LeftButton.IsEnabled = false;
+            }
+            RightButton.IsEnabled = true;
+            UpdateImage();
+        }
+
+        private void Button_Click_Next(object sender, RoutedEventArgs e)
+        {
+            currentIndex++;
+            if (currentIndex == Images.Count - 1)
+            {
+                RightButton.IsEnabled = false;
+            }
+            LeftButton.IsEnabled = true;
+            UpdateImage();
+        }
     }
 
 
