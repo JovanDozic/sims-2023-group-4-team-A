d320011a0bf905bcebf7af087bb11f8a70e5a645
AnjaMaksimovic <maksimovicanja11@gmail.com>
2023-05-12 00:18:56 +0200

    Enums, datetime and keypoint list converters created and used in views.

---
+++ sims-2023-group-4-team-A/SIMSProject/WPF/Converters/TourAppointmentStatusConverter.cs
@@ -0,0 +1,41 @@
+﻿using SIMSProject.Domain.Models.TourModels;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace SIMSProject.WPF.Converters
+{
+    public class TourAppointmentStatusConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Status status)
+            {
+                switch (status)
+                {
+                    case Status.ACTIVE:
+                        return "Aktivna";
+                    case Status.INACTIVE:
+                        return "Neaktivna";
+                    case Status.COMPLETED:
+                        return "Završena";
+                    case Status.CANCELED:
+                        return "Otkazana";
+                    default:
+                        return string.Empty;
+                }
+            }
+
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
