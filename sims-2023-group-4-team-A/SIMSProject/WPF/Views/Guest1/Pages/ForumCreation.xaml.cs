5dfe3e148d785caf275037e4713d397e2f068c68
Marko Košarac <marko_kosarac@hotmail.com>
2023-05-30 11:39:20 +0200

    Created page for forum creation

---
+++ sims-2023-group-4-team-A/SIMSProject/WPF/Views/Guest1/Pages/ForumCreation.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SIMSProject.WPF.Views.Guest1.Pages
+{
+    /// <summary>
+    /// Interaction logic for ForumCreation.xaml
+    /// </summary>
+    public partial class ForumCreation : Page
+    {
+        public ForumCreation()
+        {
+            InitializeComponent();
+            TodaysDate.Text = DateTime.Now.Date.ToString("dd.MM.yyyy.");
+        }
+    }
+}
---
+++ sims-2023-group-4-team-A/SIMSProject/WPF/Views/Guest1/Pages/Forums.xaml.cs
@@ -0,0 +1,36 @@
+﻿using SIMSProject.Domain.Models.UserModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SIMSProject.WPF.Views.Guest1.Pages
+{
+    /// <summary>
+    /// Interaction logic for Forums.xaml
+    /// </summary>
+    public partial class Forums : Page
+    {
+        private User _user = new(); 
+        public Forums(User user)
+        {
+            InitializeComponent();
+            _user = user;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new ForumCreation());
+        }
+    }
+}
